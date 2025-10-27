def3d9793d61e9315086235a24c773ebcaa69392
Deeksha Garg <gdeeksha2404@gmail.com>
2025-10-27 11:10:58 +0530

    written backend code for home page, stored user credentials to database

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,1 +1,54 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, session, Blueprint, flash
+from app import db
+from app.models import UserDetails
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+
+class Signup(FlaskForm):
+    name=StringField('Name', validators=[InputRequired('Name cannot be empty')])
+    password=PasswordField('Password', validators=[InputRequired('Name cannot be empty'), Length(min=6, message='Password must contain at least 6 characters')])
+    submit=SubmitField('Submit')
+
+login_bp=Blueprint('login', __name__)
+
+@login_bp.route('/', methods=['GET', 'POST'])
+def signup():
+    form=Signup()
+    if form.validate_on_submit():
+        name=form.name.data
+        password=form.password.data
+        new_user=UserDetails(name=name, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['user']=name
+        flash(f"Hi {name}, You have successfully signed in!", "success")
+        return redirect(url_for('fin.view_fin'))
+    return render_template('home.html')
+
+@login_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form=Signup()
+    if form.validate_on_submit():
+        name=form.name.data
+        password=form.password.data
+        
+        user=UserDetails.query.filter_by(name=name).first()
+        
+        if user and user.password==password:
+            session['user']=name
+            flash(f"Hi {name}, Welcome back!", "success")
+            return redirect(url_for('fin.view_fin'))
+        else:
+            flash("Incorrect name or password, please try again","danger")
+            return redirect(url_for('login.login'))
+        
+    return render_template('home.html')
+
+@login_bp.route('/logout')
+def logout():
+    session.pop('user')
+    flash("You logged out successfully", 'success')
+    return redirect(url_for('login.login'))
+            
+            
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import Finance
+from app.models import UserDetails
 
 app=create_app()
 
